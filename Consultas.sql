1d762fed00c2cad41bc6a745dc42cfdd5a469b27
Santiago Rivadeneira <53383986+Tiago-130414@users.noreply.github.com>
2021-10-15 13:17:30 -0600

    Agregacion de consultas y correccion de errores

--- Consultas.sql
+++ Consultas.sql
@@ -4,7 +4,7 @@ USE Proyecto2;
 porcentaje de votos en su país. Debe desplegar el nombre de la elección, el
 año de la elección, el país, el nombre del partido político y el porcentaje que
 obtuvo de votos en su país.*/
-SELECT SUB6.elec AS NombreEleccion, SUB7.ano as Año,SUB6.pais as Pais,SUB7.partido as Partido,round((SUB7.maximo/SUB6.TOTAL)*100,2) as Porcentaje FROM (
+SELECT SUB6.elec AS NombreEleccion, SUB7.ano as Año,SUB6.pais as Pais,SUB7.partido as Partido,round((SUB7.maximo/SUB6.TOTAL)*100,4) as Porcentaje FROM (
 	SELECT sum(SUB1.votos) as TOTAL,PAIS.nombre as pais,SUB1.eleccion as elec FROM PAIS
 	INNER JOIN(
 		SELECT (DETALLE_ELECCION.alfabetos + DETALLE_ELECCION.analfabetos) as votos,PAIS.idPais as pa, ELECCION.nombre as eleccion FROM DETALLE_ELECCION
@@ -62,7 +62,7 @@ INNER JOIN (
 /*Desplegar total de votos y porcentaje de votos de mujeres por departamento
 y país. El ciento por ciento es el total de votos de mujeres por país. (Tip:
 Todos los porcentajes por departamento de un país deben sumar el 100%)*/
-SELECT SUB3.pais,SUB3.departamento,SUB3.votosM, round((SUB3.votosM/SUB4.total) *100,2) as porcentaje FROM 
+SELECT SUB3.pais,SUB3.departamento,SUB3.votosM, round((SUB3.votosM/SUB4.total) *100,4) as porcentaje FROM 
 (
 	SELECT SUB1.pais as pais, SUB1.departamento as departamento,SUM(SUB1.suma) as votosM FROM 
 	(
@@ -92,47 +92,47 @@ INNER JOIN
 )SUB4 ON SUB3.pais = SUB4.pais
 ;
 
+/*CONSULTA 3*/
+/*Desplegar el nombre del país, nombre del partido político y número de
+alcaldías de los partidos políticos que ganaron más alcaldías por país.*/
+
+
+
+
 /*CONSULTA 4*/
 /*Desplegar todas las regiones por país en las que predomina la raza indígena.
 Es decir, hay más votos que las otras razas.*/
-
-SELECT SUB4.pais,SUB3.region,SUB3.tot FROM (
-	SELECT MAX(SUB1.suma) AS tot,SUB1.reg AS region,SUB1.nom AS pais FROM
+SELECT SUB4.Pais,SUB4.Region,SUB4.Total FROM 
+(
+	SELECT SUB3.Pais AS Pais,SUB3.Region AS Region,SUB3.Raza AS Raza,SUM(SUB3.total) AS Total FROM
 	(
-		SELECT PAIS.nombre as nom,REGION.nombre as reg,RAZA.descripcion AS DESCR,(DETALLE_ELECCION.alfabetos + DETALLE_ELECCION.analfabetos) as suma FROM DETALLE_ELECCION
+		SELECT PAIS.nombre AS Pais,REGION.nombre AS Region,RAZA.descripcion as Raza,(DETALLE_ELECCION.alfabetos + DETALLE_ELECCION.analfabetos) as total  FROM DETALLE_ELECCION
+		INNER JOIN RAZA ON DETALLE_ELECCION.idRaza = RAZA.idRaza
 		INNER JOIN MUNICIPIO ON DETALLE_ELECCION.idMunicipio = MUNICIPIO.idMunicipio
 		INNER JOIN DEPARTAMENTO ON DEPARTAMENTO.idDepartamento = MUNICIPIO.idDepartamento
 		INNER JOIN REGION ON REGION.idRegion = DEPARTAMENTO.idRegion
 		INNER JOIN PAIS ON PAIS.idPais = REGION.idPais
-		INNER JOIN RAZA ON DETALLE_ELECCION.idRaza = RAZA.idRaza
-		GROUP BY nom,reg,suma,RAZA.descripcion
-	)AS SUB1
-	GROUP BY SUB1.reg,SUB1.nom
-)SUB3
+	)SUB3
+	GROUP BY SUB3.Pais,SUB3.Region,SUB3.Raza
+)SUB4
 INNER JOIN
 (
-	SELECT MAX(SUB2.suma) AS tot,SUB2.nom AS pais,SUB2.DESCR as raza FROM
+	SELECT SUB2.Pais AS Pais,SUB2.Region AS Region,MAX(SUB2.total) AS Maximo FROM
 	(
-		SELECT PAIS.nombre as nom,REGION.nombre as reg,RAZA.descripcion AS DESCR,(DETALLE_ELECCION.alfabetos + DETALLE_ELECCION.analfabetos) as suma FROM DETALLE_ELECCION
-		INNER JOIN MUNICIPIO ON DETALLE_ELECCION.idMunicipio = MUNICIPIO.idMunicipio
-		INNER JOIN DEPARTAMENTO ON DEPARTAMENTO.idDepartamento = MUNICIPIO.idDepartamento
-		INNER JOIN REGION ON REGION.idRegion = DEPARTAMENTO.idRegion
-		INNER JOIN PAIS ON PAIS.idPais = REGION.idPais
-		INNER JOIN RAZA ON DETALLE_ELECCION.idRaza = RAZA.idRaza
-		GROUP BY nom,reg,suma,RAZA.descripcion
-	)AS SUB2
-	GROUP BY SUB2.nom,SUB2.DESCR
-)SUB4 ON SUB4.pais = SUB3.pais AND SUB4.tot = SUB3.tot AND SUB4.raza = 'INDIGENAS'
-;
-
-
-SELECT PAIS.nombre as nom,REGION.nombre as reg,RAZA.descripcion AS descr,(DETALLE_ELECCION.alfabetos + DETALLE_ELECCION.analfabetos) as suma FROM DETALLE_ELECCION
-INNER JOIN MUNICIPIO ON DETALLE_ELECCION.idMunicipio = MUNICIPIO.idMunicipio
-INNER JOIN DEPARTAMENTO ON DEPARTAMENTO.idDepartamento = MUNICIPIO.idDepartamento
-INNER JOIN REGION ON REGION.idRegion = DEPARTAMENTO.idRegion
-INNER JOIN PAIS ON PAIS.idPais = REGION.idPais
-INNER JOIN RAZA ON DETALLE_ELECCION.idRaza = RAZA.idRaza
-GROUP BY nom,reg,suma,RAZA.descripcion
+		SELECT SUB1.Pais as Pais,SUB1.Region as Region,SUB1.Raza as Raza,SUM(SUB1.total) as total FROM
+		(
+			SELECT PAIS.nombre AS Pais,REGION.nombre AS Region,RAZA.descripcion as Raza,(DETALLE_ELECCION.alfabetos + DETALLE_ELECCION.analfabetos) as total  FROM DETALLE_ELECCION
+			INNER JOIN RAZA ON DETALLE_ELECCION.idRaza = RAZA.idRaza
+			INNER JOIN MUNICIPIO ON DETALLE_ELECCION.idMunicipio = MUNICIPIO.idMunicipio
+			INNER JOIN DEPARTAMENTO ON DEPARTAMENTO.idDepartamento = MUNICIPIO.idDepartamento
+			INNER JOIN REGION ON REGION.idRegion = DEPARTAMENTO.idRegion
+			INNER JOIN PAIS ON PAIS.idPais = REGION.idPais
+		)SUB1
+		GROUP BY SUB1.Pais,SUB1.Region,SUB1.Raza
+	)SUB2
+	GROUP BY SUB2.Pais,SUB2.Region
+) SUB5 ON SUB5.Pais = SUB4.Pais AND SUB5.Maximo = SUB4.Total
+WHERE SUB4.Raza LIKE 'INDIGENAS'
 ;
 
 /*CONSULTA 7*/
@@ -140,27 +140,26 @@ GROUP BY nom,reg,suma,RAZA.descripcion
 departamento. Por ejemplo: si la region tiene tres departamentos, se debe
 sumar todos los votos de la region y dividirlo dentro de tres (nu´mero de
 departamentos de la region)*/
-SELECT SUB2.pais,SUB2.region, round((SUB2.total/SUB3.cantidad),2) AS promedio FROM
+
+SELECT SUB2.pais,SUB2.region, SUB2.total/SUB3.cantidad FROM 
 (
-	SELECT SUB1.pais as pais,SUB1.region as region,SUM(SUB1.suma) as total FROM
+	SELECT SUB1.Pais as pais,SUB1.Region as region,SUM(suma) as total FROM 
 	(
-		SELECT PAIS.nombre as pais,REGION.nombre as region,(DETALLE_ELECCION.alfabetos + DETALLE_ELECCION.analfabetos) as suma FROM DETALLE_ELECCION 
+		SELECT PAIS.nombre AS Pais,REGION.nombre AS Region, DEPARTAMENTO.nombre as Departamento,(DETALLE_ELECCION.alfabetos + DETALLE_ELECCION.analfabetos) as suma FROM DETALLE_ELECCION
 		INNER JOIN MUNICIPIO ON DETALLE_ELECCION.idMunicipio = MUNICIPIO.idMunicipio
 		INNER JOIN DEPARTAMENTO ON DEPARTAMENTO.idDepartamento = MUNICIPIO.idDepartamento
 		INNER JOIN REGION ON REGION.idRegion = DEPARTAMENTO.idRegion
 		INNER JOIN PAIS ON PAIS.idPais = REGION.idPais
-		GROUP BY REGION.nombre,PAIS.nombre,suma 
 	)SUB1
-	GROUP BY SUB1.pais,SUB1.region
+	GROUP BY SUB1.Pais,SUB1.Region
 )SUB2
-INNER JOIN
+INNER JOIN 
 (
-	SELECT PAIS.nombre as pais,REGION.nombre as region,COUNT(DEPARTAMENTO.idDepartamento) AS cantidad FROM MUNICIPIO 
-	INNER JOIN DEPARTAMENTO ON DEPARTAMENTO.idDepartamento = MUNICIPIO.idDepartamento
+	SELECT PAIS.nombre as pais,REGION.nombre as region,COUNT(DEPARTAMENTO.idDepartamento) as cantidad FROM DEPARTAMENTO
 	INNER JOIN REGION ON REGION.idRegion = DEPARTAMENTO.idRegion
 	INNER JOIN PAIS ON PAIS.idPais = REGION.idPais
-	GROUP BY REGION.nombre,PAIS.nombre
-) SUB3 ON  SUB2.pais = SUB3.pais AND SUB2.region = SUB3.region
+	GROUP BY PAIS.nombre,REGION.nombre
+)SUB3 ON SUB3.pais = SUB2.pais AND SUB3.region = SUB2.region
 ;
 
 /*CONSULTA 8*/
@@ -176,7 +175,7 @@ GROUP BY PAIS.nombre;
 
 /*CONSULTA 9*/
 /*Desplegar el nombre del pais y el porcentaje de votos por raza.*/
-SELECT SUB3.pais,SUB3.raza,round((SUB3.total/SUB4.total)* 100,2) AS porcentaje FROM 
+SELECT SUB3.pais,SUB3.raza,round((SUB3.total/SUB4.total)* 100,4) AS porcentaje FROM 
 (
 	SELECT SUM(suma) as total, SUB1.pais as pais,SUB1.descr as raza FROM
 	(
@@ -186,7 +185,6 @@ SELECT SUB3.pais,SUB3.raza,round((SUB3.total/SUB4.total)* 100,2) AS porcentaje F
 		INNER JOIN REGION ON REGION.idRegion = DEPARTAMENTO.idRegion
 		INNER JOIN PAIS ON PAIS.idPais = REGION.idPais
 		INNER JOIN RAZA ON DETALLE_ELECCION.idRaza = RAZA.idRaza
-		GROUP BY pais,suma,RAZA.descripcion
 	)SUB1
 	GROUP BY SUB1.pais,SUB1.descr
 )SUB3
@@ -200,10 +198,106 @@ INNER JOIN
 		INNER JOIN REGION ON REGION.idRegion = DEPARTAMENTO.idRegion
 		INNER JOIN PAIS ON PAIS.idPais = REGION.idPais
 		INNER JOIN RAZA ON DETALLE_ELECCION.idRaza = RAZA.idRaza
-		GROUP BY pais,suma,RAZA.descripcion
 	)SUB2
 	GROUP BY SUB2.pais
 )SUB4 ON SUB4.pais = SUB3.pais
 ;
 
+/*CONSULTA 10*/
+/*Desplegar el nombre del país en el cual las elecciones han sido más
+peleadas. Para determinar esto se debe calcular la diferencia de porcentajes
+de votos entre el partido que obtuvo más votos y el partido que obtuvo menos
+votos.*/
+
+
+
+
 /*CONSULTA 11*/
+/*Desplegar el total de votos y el porcentaje de votos emitidos por mujeres
+indígenas alfabetas.*/
+
+SELECT SUB1.Pais,SUB1.Votos,Round((SUB1.Votos/SUB2.Votos)*100,2) AS Porcentaje FROM 
+(
+	SELECT PAIS.nombre AS Pais,SUM(DETALLE_ELECCION.alfabetos) AS Votos FROM DETALLE_ELECCION
+	INNER JOIN SEXO ON SEXO.idSexo = DETALLE_ELECCION.idSexo AND SEXO.descripcion LIKE 'mujeres'
+	INNER JOIN RAZA ON RAZA.idRaza = DETALLE_ELECCION.idRaza AND RAZA.descripcion LIKE 'INDIGENAS'
+	INNER JOIN MUNICIPIO ON  MUNICIPIO.idMunicipio = DETALLE_ELECCION.idMunicipio
+	INNER JOIN DEPARTAMENTO ON DEPARTAMENTO.idDepartamento = MUNICIPIO.idDepartamento
+	INNER JOIN REGION ON REGION.idRegion = DEPARTAMENTO.idRegion
+	INNER JOIN PAIS ON PAIS.idPais = REGION.idPais
+	GROUP BY PAIS.nombre,SEXO.descripcion
+)SUB1
+INNER JOIN (
+	SELECT SUB3.Pais AS Pais,SUM(SUB3.Total) AS Votos FROM
+	(
+		SELECT PAIS.nombre AS Pais,(DETALLE_ELECCION.alfabetos + DETALLE_ELECCION.analfabetos) AS Total FROM DETALLE_ELECCION
+		INNER JOIN MUNICIPIO ON  MUNICIPIO.idMunicipio = DETALLE_ELECCION.idMunicipio
+		INNER JOIN DEPARTAMENTO ON DEPARTAMENTO.idDepartamento = MUNICIPIO.idDepartamento
+		INNER JOIN REGION ON REGION.idRegion = DEPARTAMENTO.idRegion
+		INNER JOIN PAIS ON PAIS.idPais = REGION.idPais
+	)SUB3
+	GROUP BY SUB3.Pais
+)SUB2 ON SUB1.Pais = SUB2.Pais
+;
+
+/*CONSULTA 12*/
+/*Desplegar el nombre del país, el porcentaje de votos de ese país en el que
+han votado mayor porcentaje de analfabetas. (tip: solo desplegar un nombre
+de país, el de mayor porcentaje).*/
+
+SELECT SUB1.Pais AS Pais,Round((SUB1.Votos/SUB2.Votos)*100,4) AS Porcentaje FROM 
+(
+	SELECT PAIS.nombre AS Pais,SUM(DETALLE_ELECCION.analfabetos) AS Votos FROM DETALLE_ELECCION
+	INNER JOIN MUNICIPIO ON  MUNICIPIO.idMunicipio = DETALLE_ELECCION.idMunicipio
+	INNER JOIN DEPARTAMENTO ON DEPARTAMENTO.idDepartamento = MUNICIPIO.idDepartamento
+	INNER JOIN REGION ON REGION.idRegion = DEPARTAMENTO.idRegion
+	INNER JOIN PAIS ON PAIS.idPais = REGION.idPais
+	GROUP BY PAIS.nombre
+)SUB1
+INNER JOIN (
+	SELECT SUB3.Pais AS Pais,SUM(SUB3.Total) AS Votos FROM
+	(
+		SELECT PAIS.nombre AS Pais,(DETALLE_ELECCION.alfabetos + DETALLE_ELECCION.analfabetos) AS Total FROM DETALLE_ELECCION
+		INNER JOIN MUNICIPIO ON  MUNICIPIO.idMunicipio = DETALLE_ELECCION.idMunicipio
+		INNER JOIN DEPARTAMENTO ON DEPARTAMENTO.idDepartamento = MUNICIPIO.idDepartamento
+		INNER JOIN REGION ON REGION.idRegion = DEPARTAMENTO.idRegion
+		INNER JOIN PAIS ON PAIS.idPais = REGION.idPais
+	)SUB3
+	GROUP BY SUB3.Pais
+)SUB2 ON SUB1.Pais = SUB2.Pais
+ORDER BY Porcentaje DESC LIMIT 1
+;
+
+/*CONSULTA 13*/
+/*Desplegar la lista de departamentos de Guatemala y número de votos
+obtenidos, para los departamentos que obtuvieron más votos que el
+departamento de Guatemala.*/
+SELECT SUB4.Pais,SUB4.Departamento,SUB4.Total FROM
+(
+	SELECT SUB1.Pais AS pais,SUB1.departamento AS Departamento,SUM(SUB1.Total) as Total FROM 
+	(
+		SELECT PAIS.nombre AS pais,DEPARTAMENTO.nombre as departamento,(DETALLE_ELECCION.alfabetos + DETALLE_ELECCION.analfabetos) AS Total  FROM DETALLE_ELECCION
+		INNER JOIN MUNICIPIO ON MUNICIPIO.idMunicipio = DETALLE_ELECCION.idMunicipio
+		INNER JOIN DEPARTAMENTO ON DEPARTAMENTO.idDepartamento = MUNICIPIO.idDepartamento
+		INNER JOIN REGION ON REGION.idRegion = DEPARTAMENTO.idRegion
+		INNER JOIN PAIS ON PAIS.idPais = REGION.idPais
+	)SUB1
+	WHERE SUB1.pais LIKE 'GUATEMALA' AND departamento LIKE 'Guatemala'
+	GROUP BY SUB1.pais,SUB1.departamento
+)SUB3
+INNER JOIN (
+	SELECT SUB2.Pais AS Pais,SUB2.departamento AS Departamento,SUM(SUB2.Total) as Total FROM 
+	(
+		SELECT PAIS.nombre AS pais,DEPARTAMENTO.nombre as departamento,(DETALLE_ELECCION.alfabetos + DETALLE_ELECCION.analfabetos) AS Total  FROM DETALLE_ELECCION
+		INNER JOIN MUNICIPIO ON MUNICIPIO.idMunicipio = DETALLE_ELECCION.idMunicipio
+		INNER JOIN DEPARTAMENTO ON DEPARTAMENTO.idDepartamento = MUNICIPIO.idDepartamento
+		INNER JOIN REGION ON REGION.idRegion = DEPARTAMENTO.idRegion
+		INNER JOIN PAIS ON PAIS.idPais = REGION.idPais
+	)SUB2
+	WHERE SUB2.pais LIKE 'GUATEMALA'
+	GROUP BY SUB2.pais,SUB2.departamento
+)SUB4 ON SUB4.Pais = SUB3.Pais
+WHERE SUB4.Total> SUB3.Total
+;
+
+
